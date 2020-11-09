84d9c5968afd21218bc39757834124ce9fcfdf71
macbook <alanw5660@gmail.com>
2020-11-09 13:33:46 +0700

    HappyPlaces Project

---
+++ app/src/main/java/com/adith/blast/happyplace/activity/MapsActivity.kt
@@ -0,0 +1,46 @@
+package com.adith.blast.happyplace.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adith.blast.happyplace.R
+import com.adith.blast.happyplace.models.HappyPlaceModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_map.*
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var mHappyPlaceDetail: HappyPlaceModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        mHappyPlaceDetail = intent.getParcelableExtra(MainActivity.EXTRA_PLACE_DETAIL)
+
+        mHappyPlaceDetail.let {
+            if (it != null){
+                setSupportActionBar(toolbar_map)
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                supportActionBar?.title = it?.title
+
+                toolbar_map.setNavigationOnClickListener {
+                    onBackPressed()
+                }
+
+                val supportMapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+                supportMapFragment.getMapAsync(this)
+            }
+        }
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        val position = LatLng(mHappyPlaceDetail!!.latitude, mHappyPlaceDetail!!.longitude)
+        map!!.addMarker(MarkerOptions().position(position).title(mHappyPlaceDetail!!.location))
+        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(position, 20f)
+        map.animateCamera(newLatLngZoom)
+    }
+}
